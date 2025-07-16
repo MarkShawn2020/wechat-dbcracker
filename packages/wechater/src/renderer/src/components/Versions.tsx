b775f4738296c5327cf68e2951c7079a44c2221f
markshawn2020 <shawninjuly@gmail.com>
2025-07-17 01:13:27 +0800

    add wechat-db-manager

--- packages/wechater/src/renderer/src/components/Versions.tsx
+++
@@ -1,9 +0,0 @@
-import { version } from '@renderer/version'
-
-export function Versions(): JSX.Element {
-  return (
-    <div className="versions">
-      <span>Wechater v{version}</span>
-    </div>
-  )
-}
